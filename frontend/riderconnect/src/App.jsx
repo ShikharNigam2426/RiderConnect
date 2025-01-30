fd9379f4b7a14465594194020618327f7fb681e0
ShikharNigam2426 <shikharnigam123@gmail.com>
2025-01-30 11:17:37 +0530

    first commit

---
+++ frontend/riderconnect/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Components/Navbar";
+import "./index.css"; // Tailwind CSS ko enable karne ke liye
+
+const App = () => {
+  return (
+    <Navbar />
+  );
+};
+
+export default App;
